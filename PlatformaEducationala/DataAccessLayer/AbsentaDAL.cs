2d691a431e2e249c9c9b063c41bb9df51583e772
AlinaPastor <pastor_alina_ada@yahoo.com>
2014-03-25 20:09:25 +0200

    Clasa care acceseaza baza de date prin intermediul procedurilor stocate definite, pentru a retuna absentele care respecta anumite criterii.

---
+++ PlatformaEducationala/DataAccessLayer/AbsentaDAL.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace PlatformaEducationala.DataAccessLayer
+{
+    class AbsentaDAL
+    {
+        DatabaseAccess db = new DatabaseAccess();
+
+        public DataSet GetAbsenteByIDuri(int id_sem, int id_elev, int id_materie)
+        {
+            SqlParameter[] paramList = new SqlParameter[]
+            {
+                 new SqlParameter("@semestruID", id_sem),
+                 new SqlParameter("@elevID",id_elev),
+                 new SqlParameter("@materieID",id_materie )
+            };
+            return db.ExecuteDataSet(DALHelper.Connection, "GetAbsenteByIDuri", paramList);
+        }
+
+        public DataSet GetAbsenteByIDElev(int id_elev)
+        {
+            SqlParameter[] paramList = new SqlParameter[]
+            {
+                
+                 new SqlParameter("@elevID",id_elev)
+            };
+            return db.ExecuteDataSet(DALHelper.Connection, "GetAbsenteByIDElev", paramList);
+        }
+
+        public DataSet GetAbsenteNemotivateByIDElev(int id_elev)
+        {
+            SqlParameter[] paramList = new SqlParameter[]
+            {
+                
+                 new SqlParameter("@elevID",id_elev)
+            };
+            return db.ExecuteDataSet(DALHelper.Connection, "GetAbsenteNemotivateByIDElev", paramList);
+        }
+
+        public DataSet GetAbsenteClasa(int id_clasa)
+        {
+            SqlParameter[] paramList = new SqlParameter[]
+            {
+          
+                 new SqlParameter("@clasaID",id_clasa )
+            };
+            return db.ExecuteDataSet(DALHelper.Connection, "GetAbsenteClasa", paramList);
+        }
+
+        public DataSet GetAbsenteClasaNemotivate(int id_clasa)
+        {
+            SqlParameter[] paramList = new SqlParameter[]
+            {
+          
+                 new SqlParameter("@clasaID",id_clasa )
+            };
+            return db.ExecuteDataSet(DALHelper.Connection, "GetAbsenteClasaNemotivate", paramList);
+        } 
+        internal void AddAbsenta(int id_sem, int id_elev, int id_materie, bool motivata, bool motivabila)
+        {
+            SqlParameter[] paramList = new SqlParameter[]
+            {
+                
+                new SqlParameter("@semestruID", id_sem),
+                new SqlParameter("@elevID", id_elev),
+                new SqlParameter("@materieID", id_materie),
+                new SqlParameter("@motivata",motivata),
+                new SqlParameter("@motivabila",motivabila)
+
+            };
+            db.ExecuteNonQuery(DALHelper.Connection, "AddAbsenta", paramList);
+        }
+
+        internal void MotivareAbsenta(int id_absenta)
+        {
+            SqlParameter[] paramList = new SqlParameter[]
+            {
+                new SqlParameter("@absentaID", id_absenta)
+               
+
+            };
+            db.ExecuteNonQuery(DALHelper.Connection, "MotivareAbsenta", paramList);
+        }
+
+        public DataSet GetMotivabilitateById(int id_absenta)
+        {
+            SqlParameter[] paramList = new SqlParameter[]
+            {
+                 new SqlParameter("@absentaID", id_absenta)
+                
+            };
+            return db.ExecuteDataSet(DALHelper.Connection, "GetMotivabilitateById", paramList);
+        }
+
+    }
+
+}
