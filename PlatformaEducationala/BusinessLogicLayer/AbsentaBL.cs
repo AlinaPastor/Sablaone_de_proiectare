cf5b7c707b2c4d89b0f9b8c4cf972506e1eaafe5
AlinaPastor <pastor_alina_ada@yahoo.com>
2014-03-25 19:48:34 +0200

    Ofera posibilitatea lucrului cu absente. Mai exact, returneaza absentele unui elev in functie de diferite criterii date la nivel de BLL (Business Logic Layer).

---
+++ PlatformaEducationala/BusinessLogicLayer/AbsentaBL.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using PlatformaEducationala.DataAccessLayer;
+using System.Data;
+
+namespace PlatformaEducationala.BusinessLogicLayer
+{
+    class AbsentaBL
+    {
+        AbsentaDAL absDAL = new AbsentaDAL();
+
+        public DataSet GetAbsenteByIDuri(int id_sem, int id_elev, int id_materie)
+        {
+            return absDAL.GetAbsenteByIDuri(id_sem,  id_elev, id_materie);
+        }
+
+        public DataSet GetAbsenteByIDElev(int id_elev)
+        {
+            return absDAL.GetAbsenteByIDElev(id_elev);
+        }
+
+        public DataSet GetAbsenteNemotivateByIDElev(int id_elev)
+        {
+            return absDAL.GetAbsenteNemotivateByIDElev(id_elev);
+        }
+
+
+        public DataSet GetAbsenteClasa(int id_clasa)
+        {
+            return absDAL.GetAbsenteClasa(id_clasa);
+        }
+
+        public DataSet GetAbsenteClasaNemotivate(int id_clasa)
+        {
+            return absDAL.GetAbsenteClasaNemotivate(id_clasa);
+        }
+
+        internal void AddAbsenta(int id_sem, int id_elev, int id_materie,  bool motivata, bool motivabila)
+        {
+            absDAL.AddAbsenta( id_sem, id_elev, id_materie, motivata, motivabila);
+        }
+
+        internal void MotivareAbsenta(int id_absenta)
+        {
+           absDAL.MotivareAbsenta(id_absenta);
+        }
+
+        public DataSet GetMotivabilitateById(int id_absenta)
+        {
+            return absDAL.GetMotivabilitateById(id_absenta);
+        }
+
+
+    }
+}
