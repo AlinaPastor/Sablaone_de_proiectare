7c49f4e475e4c8521956a26457c593d99f07f0ef
AlinaPastor <pastor_alina_ada@yahoo.com>
2014-03-26 22:38:33 +0200

    Realizeaza operatiile necesare cat si introducerea mediei semstriale in baza de date pentru un anumit elev utilizand proceduri stocate

---
+++ PlatformaEducationala/DataAccessLayer/MedieSemestruDAL.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace PlatformaEducationala.DataAccessLayer
+{
+    class MedieSemestruDAL
+    {
+        DatabaseAccess db = new DatabaseAccess();
+        public DataSet GetMedieSemByIDuri(int id_sem, int id_elev, int id_materie)
+        {
+            SqlParameter[] paramList = new SqlParameter[]
+            {
+                 new SqlParameter("@semestruID", id_sem),
+                 new SqlParameter("@elevID",id_elev),
+                 new SqlParameter("@materieID",id_materie )
+            };
+            return db.ExecuteDataSet(DALHelper.Connection, "GetMedieSemByIDuri", paramList);
+        }
+
+        internal void AddMedie(int id_elev, int id_materie, int id_sem, int media)
+        {
+            SqlParameter[] paramList = new SqlParameter[]
+            {
+                
+                new SqlParameter("@semestruID", id_sem),
+                new SqlParameter("@elevID", id_elev),
+                new SqlParameter("@materieID", id_materie),
+                 new SqlParameter("@medie",media)
+
+            };
+            db.ExecuteNonQuery(DALHelper.Connection, "AddMedie", paramList);
+        }
+    }
+}
