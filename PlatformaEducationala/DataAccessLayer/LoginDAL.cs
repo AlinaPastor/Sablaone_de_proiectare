c61698a09b2c0b61ac6e09fdc43b0a65d31886e1
AlinaPastor <pastor_alina_ada@yahoo.com>
2014-03-26 21:58:16 +0200

    Clasa  ce verifica corectitudinea numelui de utilizator si a parolei introduse de utilizator la login.

---
+++ PlatformaEducationala/DataAccessLayer/LoginDAL.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace PlatformaEducationala.DataAccessLayer
+{
+   public class LoginDAL
+    {
+        DatabaseAccess db = new DatabaseAccess();
+
+        internal DataSet LoginProfesor(string user, string parola)
+        {
+            SqlParameter[] paramList = new SqlParameter[]
+            {
+                new SqlParameter("@user", user),
+                new SqlParameter("@parola", parola)
+            };
+           return db.ExecuteDataSet(DALHelper.Connection, "LoginProfesor", paramList);
+        }
+
+        internal DataSet LoginDiriginte(string user, string parola)
+        {
+            SqlParameter[] paramList = new SqlParameter[]
+            {
+                new SqlParameter("@user", user),
+                new SqlParameter("@parola", parola)
+            };
+            return db.ExecuteDataSet(DALHelper.Connection, "LoginDiriginte", paramList);
+        }
+
+        internal DataSet LoginElev(string user, string parola)
+        {
+            SqlParameter[] paramList = new SqlParameter[]
+            {
+                new SqlParameter("@user", user),
+                new SqlParameter("@parola", parola)
+            };
+           return db.ExecuteDataSet(DALHelper.Connection, "LoginElev", paramList);
+        }
+
+        internal DataSet LoginAdmin(string user, string parola)
+        {
+            SqlParameter[] paramList = new SqlParameter[]
+            {
+                new SqlParameter("@user", user),
+                new SqlParameter("@parola", parola)
+            };
+            return db.ExecuteDataSet(DALHelper.Connection, "LoginAdmin", paramList);
+        }
+
+    }
+}
