c170139d5f9fd57c13846651a078fa90b291c295
AlinaPastor <pastor_alina_ada@yahoo.com>
2014-03-26 22:34:37 +0200

    Clasa care realizeaza operatii de CRUD pe profesori in baza de date.

---
+++ PlatformaEducationala/DataAccessLayer/ProfesorDAL.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace PlatformaEducationala.DataAccessLayer
+{
+    class ProfesorDAL
+    {
+        DatabaseAccess db = new DatabaseAccess();
+
+        public DataSet GetProfesori()
+        {
+            return db.ExecuteDataSet(DALHelper.Connection, "GetProfesori", null);
+        }
+
+        public DataSet GetProfesoriById(int profesorID)
+        {
+            SqlParameter[] paramList = new SqlParameter[]
+            {
+                new SqlParameter("@profesorID",profesorID )
+            };
+            return db.ExecuteDataSet(DALHelper.Connection, "GetProfesoriById", paramList);
+        }
+
+        public DataSet GetProfesoriByMaterieId(int materieID)
+        {
+            SqlParameter[] paramList = new SqlParameter[]
+            {
+                new SqlParameter("@materieID",materieID )
+            };
+            return db.ExecuteDataSet(DALHelper.Connection, "GetProfesoriByMaterieId", paramList);
+        }
+        internal void AddProfesor(string nume, string prenume, string user, string parola)
+        {
+            SqlParameter[] paramList = new SqlParameter[]
+            {
+                
+                new SqlParameter("@nume", nume),
+                new SqlParameter("@prenume", prenume),
+                new SqlParameter("@user", user),
+                new SqlParameter("@parola", parola)
+
+            };
+            db.ExecuteNonQuery(DALHelper.Connection, "AddProfesor", paramList);
+        }
+
+
+        internal void UpdateProfesor(int id_profesor, string nume, string prenume, string user, string parola)
+        {
+            SqlParameter[] paramList = new SqlParameter[]
+            {
+                new SqlParameter("@profesorID", id_profesor),
+                new SqlParameter("@nume", nume),
+                new SqlParameter("@prenume", prenume),
+                new SqlParameter("@user", user),
+                new SqlParameter("@parola", parola)
+
+            };
+            db.ExecuteNonQuery(DALHelper.Connection, "UpdateProfesor", paramList);
+        }
+
+        internal void DeleteProfesor(int profesorID)
+        {
+            SqlParameter[] paramList = new SqlParameter[]
+            {
+                new SqlParameter("@profesorID", profesorID)
+            };
+            db.ExecuteNonQuery(DALHelper.Connection, "DeleteProfesor", paramList);
+        }
+
+        internal void AsocProfesorMaterie(int id_profesor, int id_materie)
+        {
+            SqlParameter[] paramList = new SqlParameter[]
+            {
+                new SqlParameter("@profesorID", id_profesor),
+                new SqlParameter("@materieID", id_materie)
+               
+
+            };
+            db.ExecuteNonQuery(DALHelper.Connection, "AsocProfesorMaterie", paramList);
+        }
+
+        public DataSet GetIdForProfMaterie( int id_materie, int id_profesor)
+        {
+            SqlParameter[] paramList = new SqlParameter[]
+            {
+                 new SqlParameter("@materieID", id_materie),
+                new SqlParameter("@profesorID", id_profesor)
+               
+            };
+            return db.ExecuteDataSet(DALHelper.Connection, "GerIdForProfMaterie", paramList);
+        }
+
+
+        internal void UpdateAsocProfesorMaterie(int id_prof_materie,  int id_profesor)
+        {
+            SqlParameter[] paramList = new SqlParameter[]
+            {
+                new SqlParameter("@prof_materieID", id_prof_materie),
+               // new SqlParameter("@materieID", id_materie),
+                new SqlParameter("@profesorID", id_profesor)
+               
+
+            };
+            db.ExecuteNonQuery(DALHelper.Connection, "UpdateAscoProfesorMaterie", paramList);
+        }
+    }
+}
