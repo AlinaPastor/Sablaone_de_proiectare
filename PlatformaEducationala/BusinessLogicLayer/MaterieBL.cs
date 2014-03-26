bc9755dc5c857bd3fc690a11ba5f7354f6b2b2cc
AlinaPastor <pastor_alina_ada@yahoo.com>
2014-03-26 22:28:19 +0200

    Clasa responsabila cu informatiile despre o anumita disciplina la nivel de BLL

---
+++ PlatformaEducationala/BusinessLogicLayer/MaterieBL.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using PlatformaEducationala.DataAccessLayer;
+using System.Data;
+
+namespace PlatformaEducationala.BusinessLogicLayer
+{
+    class MaterieBL
+    {
+
+        MaterieDAL materieDAL = new MaterieDAL();
+        public DataSet GetMaterii()
+        {
+            return materieDAL.GetMaterii();
+        }
+
+        public DataSet GetMaterieById(int materieID)
+        {
+            return materieDAL.GetMaterieById(materieID);
+        }
+
+        internal void AddMaterie(string nume)
+        {
+            materieDAL.AddMaterie(nume);
+        }
+
+        internal void UpdateMaterie(int materieID, string nume)
+        {
+            materieDAL.UpdateMaterie(materieID, nume);
+        }
+
+        internal void DeleteMaterie(int materieID)
+        {
+            materieDAL.DeleteMaterie(materieID);
+        }
+
+        public DataSet GetMaterieByProfId(int profID)
+        {
+            return materieDAL.GetMaterieByProfId(profID);
+        }
+    }
+}
