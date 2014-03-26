b8f27e122c4ca59f894ea1a83adc0c3fa55362a5
AlinaPastor <pastor_alina_ada@yahoo.com>
2014-03-26 22:40:06 +0200

    Clasa ce faciliteaza lucrul cu procedurile stocate din baza de date.

---
+++ PlatformaEducationala/DataAccessLayer/DatabaseAccess.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace PlatformaEducationala.DataAccessLayer
+{
+    class DatabaseAccess
+    {
+        public void ExecuteNonQuery(SqlConnection conn, String storedProcedureName, SqlParameter[] paramsList)
+        {
+            try
+            {
+                SqlCommand cmd = new SqlCommand(storedProcedureName, conn);
+                cmd.CommandType = System.Data.CommandType.StoredProcedure;
+                if (paramsList != null)
+                {
+                    foreach (SqlParameter param in paramsList)
+                    {
+                        cmd.Parameters.Add(param);
+                    }
+                }
+                conn.Open();
+                cmd.ExecuteNonQuery();
+            }
+            finally
+            {
+                conn.Close();
+            }
+        }
+        public DataSet ExecuteDataSet(SqlConnection conn, String storedProcedureName, SqlParameter[] paramsList)
+        {
+            DataSet result = new DataSet();
+            try
+            {
+                SqlCommand cmd = new SqlCommand(storedProcedureName, conn);
+                cmd.CommandType = CommandType.StoredProcedure;
+                if (paramsList != null)
+                {
+                    foreach (SqlParameter param in paramsList)
+                    {
+                        cmd.Parameters.Add(param);
+                    }
+                }
+                conn.Open();
+                SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+                adapter.Fill(result);
+                adapter.Dispose();
+            }
+            finally
+            {
+                conn.Close();
+            }
+            return result;
+        }
+    }
+}
