b8bce52796fb4b372a934eda4267a3f8d33adada
AlinaPastor <pastor_alina_ada@yahoo.com>
2014-03-26 22:39:17 +0200

    Face legatura efectiva dintre program si baza de date

---
+++ PlatformaEducationala/DataAccessLayer/DALHelper.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+//using System.Linq;
+using System.Text;
+using System.Configuration;
+using System.Data.SqlClient;
+
+namespace PlatformaEducationala.DataAccessLayer
+{
+    static class DALHelper
+    {
+        private static readonly String connectionString = ConfigurationManager.ConnectionStrings["connectionString"].ConnectionString;
+
+        internal static SqlConnection Connection
+        {
+            get
+            {
+                return new SqlConnection(connectionString);
+            }
+        }
+    }
+}
