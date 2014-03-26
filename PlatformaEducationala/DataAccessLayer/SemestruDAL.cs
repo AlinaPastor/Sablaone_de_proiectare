c6a800ed2b1f152d6ea500435aa360f7bbea3516
AlinaPastor <pastor_alina_ada@yahoo.com>
2014-03-26 22:36:05 +0200

    Realizeaza operatii in baza de date la nivel de semstru cu ajutorul procedurilor stocate existente

---
+++ PlatformaEducationala/DataAccessLayer/SemestruDAL.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace PlatformaEducationala.DataAccessLayer
+{
+    class SemestruDAL
+    {
+        DatabaseAccess db = new DatabaseAccess();
+
+        public DataSet GetSemestre()
+        {
+            return db.ExecuteDataSet(DALHelper.Connection, "GetSemestre", null);
+        }
+
+        public DataSet GetSemestruById(int semID)
+        {
+            SqlParameter[] paramList = new SqlParameter[]
+            {
+                new SqlParameter("@semID",semID )
+            };
+            return db.ExecuteDataSet(DALHelper.Connection, "GetSemestreById", paramList);
+        }
+
+        internal void AsocAn_StudiuSemestru(int id_sem, int id_an)
+        {
+            SqlParameter[] paramList = new SqlParameter[]
+            {
+                new SqlParameter("@semID", id_sem),
+                new SqlParameter("@an_studiuID", id_an),
+             
+            };
+            db.ExecuteNonQuery(DALHelper.Connection, "AsocAn_StudiuSemestru", paramList);
+        }
+
+    }
+}
