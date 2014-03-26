2793ec65d3087f6103c7787b4805f355f953962b
AlinaPastor <pastor_alina_ada@yahoo.com>
2014-03-26 21:42:51 +0200

    Clasa care ne ofera informatii despre elevi la nicel de BLL.

---
+++ PlatformaEducationala/BusinessLogicLayer/ElevBL.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using PlatformaEducationala.DataAccessLayer;
+using System.Data;
+
+namespace PlatformaEducationala.BusinessLogicLayer
+{
+    class ElevBL
+    {
+        ElevDAL elevDAL = new ElevDAL();
+        public DataSet GetElevi()
+        {
+            return elevDAL.GetElevi();
+        }
+
+        public DataSet GetEleviFaraClasa()
+        {
+            return elevDAL.GetEleviFaraClasa();
+        }
+
+        public DataSet GetEleviById(int elevID)
+        {
+            return elevDAL.GetEleviById(elevID);
+        }
+
+        internal void AddElev(string nume, string prenume, string user, string parola)
+        {
+            elevDAL.AddElev(nume, prenume, user, parola);
+        }
+
+        internal void UpdateElev(int elevID, string nume, string prenume, string user, string parola)
+        {
+            elevDAL.UpdateElev(elevID, nume, prenume, user, parola);
+        }
+
+
+        internal void DeleteElev(int elevID)
+        {
+            elevDAL.DeleteElev(elevID);
+        }
+
+        internal void AsocElevClasa(int elevID, int clasaID)
+        {
+            elevDAL.AsocElevClasa(elevID,clasaID);
+        }
+
+        public DataSet GetEleviByClasaId(int clasaID)
+        {
+            return elevDAL.GetEleviByClasaId(clasaID);
+        }
+
+
+    }
+}
