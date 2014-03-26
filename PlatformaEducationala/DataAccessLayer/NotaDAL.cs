5a6afd25bbc3f90c475396a7e14482f17cdcf731
AlinaPastor <pastor_alina_ada@yahoo.com>
2014-03-26 22:30:32 +0200

    Clasa ce realizeaza operatii de CRUD pe baza de date cu ajutorul unor proceduri stocate la nivel de nota.

---
+++ PlatformaEducationala/DataAccessLayer/NotaDAL.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace PlatformaEducationala.DataAccessLayer
+{
+    class NotaDAL
+    {
+
+        DatabaseAccess db = new DatabaseAccess();
+
+        public DataSet GetNoteByIDuri(int id_elev, int id_materie, int id_sem)
+        {
+            SqlParameter[] paramList = new SqlParameter[]
+            {
+                 new SqlParameter("@elevID",id_elev),
+                 new SqlParameter("@materieID",id_materie),
+                 new SqlParameter("@semestruID", id_sem)
+                
+                 
+            };
+            return db.ExecuteDataSet(DALHelper.Connection, "GetNoteByIDuri", paramList);
+        }
+
+        public DataSet GetNoteByIDuri1(int id_elev, int id_materie, int id_sem)
+        {
+            SqlParameter[] paramList = new SqlParameter[]
+            {
+                 new SqlParameter("@elevID",id_elev),
+                 new SqlParameter("@materieID",id_materie),
+                 new SqlParameter("@semestruID", id_sem)
+                
+                 
+            };
+            return db.ExecuteDataSet(DALHelper.Connection, "GetNoteByIDuri1", paramList);
+        }
+
+        public DataSet GetTezaByIDuri(int id_elev, int id_materie, int id_sem)
+        {
+            SqlParameter[] paramList = new SqlParameter[]
+            {
+                 new SqlParameter("@elevID",id_elev),
+                 new SqlParameter("@materieID",id_materie),
+                 new SqlParameter("@semestruID", id_sem)
+                
+                 
+            };
+            return db.ExecuteDataSet(DALHelper.Connection, "GetTezaByIDuri", paramList);
+        }
+
+        internal void AddNota(int id_elev, int id_materie, int id_sem, int nota)
+        {
+            SqlParameter[] paramList = new SqlParameter[]
+            {
+                new SqlParameter("@elevID", id_elev),
+                new SqlParameter("@materieID", id_materie),
+                new SqlParameter("@semestruID", id_sem),
+                new SqlParameter("@nota",nota)
+
+            };
+            db.ExecuteNonQuery(DALHelper.Connection, "AddNota", paramList);
+        }
+
+        internal void DeleteNota(int id_nota)
+        {
+            SqlParameter[] paramList = new SqlParameter[]
+            {
+                new SqlParameter("@notaID", id_nota)
+               
+
+            };
+            db.ExecuteNonQuery(DALHelper.Connection, "DeleteNota", paramList);
+        }
+
+        internal void AddTeza(int id_elev, int id_materie, int id_sem, int teza)
+        {
+            SqlParameter[] paramList = new SqlParameter[]
+            {
+                new SqlParameter("@elevID", id_elev),
+                new SqlParameter("@materieID", id_materie),
+                new SqlParameter("@semestruID", id_sem),
+                new SqlParameter("@teza",teza)
+
+            };
+            db.ExecuteNonQuery(DALHelper.Connection, "AddTeza", paramList);
+        }
+
+    }
+}
