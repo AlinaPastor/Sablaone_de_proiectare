d3907749007c163cdfe06084620828d4faafb87b
AlinaPastor <pastor_alina_ada@yahoo.com>
2014-03-26 21:38:12 +0200

    Clasa responsabila cu CRUD asupra anilor de studiu la nivel de baza de date.

---
+++ PlatformaEducationala/DataAccessLayer/An_StudiuDAL.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace PlatformaEducationala.DataAccessLayer
+{
+    class An_StudiuDAL
+    {
+        DatabaseAccess db = new DatabaseAccess();
+
+        public DataSet GetAni_Studiu()
+        {
+            return db.ExecuteDataSet(DALHelper.Connection, "GetAni_Studiu", null);
+        }
+
+        public DataSet GetAn_StudiuById(int an_studiuID)
+        {
+            SqlParameter[] paramList = new SqlParameter[]
+            {
+                new SqlParameter("@an_studiuID",an_studiuID )
+            };
+            return db.ExecuteDataSet(DALHelper.Connection, "GetAn_StudiuById", paramList);
+        }
+
+        internal void AddAn_Studiu(string an)
+        {
+            SqlParameter[] paramList = new SqlParameter[]
+            {
+                
+                new SqlParameter("@an", an)
+            };
+            db.ExecuteNonQuery(DALHelper.Connection, "AddAn_Studiu", paramList);
+        }
+
+
+        internal void UpdateAn_Studiu(int id_an_studiu, string an)
+        {
+            SqlParameter[] paramList = new SqlParameter[]
+            {
+                new SqlParameter("@an_studiuID", id_an_studiu),
+                new SqlParameter("@an", an)
+                
+
+            };
+            db.ExecuteNonQuery(DALHelper.Connection, "UpdateAn_Studiu", paramList);
+        }
+
+        internal void DeleteAn_Studiu(int an_studiuID)
+        {
+            SqlParameter[] paramList = new SqlParameter[]
+            {
+                new SqlParameter("@an_studiuID", an_studiuID)
+            };
+            db.ExecuteNonQuery(DALHelper.Connection, "DeleteAn_Studiu", paramList);
+        }
+
+       
+
+    }
+}
