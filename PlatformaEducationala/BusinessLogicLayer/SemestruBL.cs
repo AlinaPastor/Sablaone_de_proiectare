fa79535d45978c8764746fc74721331d78e901da
AlinaPastor <pastor_alina_ada@yahoo.com>
2014-03-26 22:35:04 +0200

    Defineste comporatmentul la nicel de semstru (BLL)

---
+++ PlatformaEducationala/BusinessLogicLayer/SemestruBL.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using PlatformaEducationala.DataAccessLayer;
+using System.Data;
+
+namespace PlatformaEducationala.BusinessLogicLayer
+{
+    class SemestruBL
+    {
+        SemestruDAL semDAL = new SemestruDAL();
+        public DataSet GetSemestre()
+        {
+            return semDAL.GetSemestre();
+        }
+
+        public DataSet GetSemestruById(int semID)
+        {
+            return semDAL.GetSemestruById(semID);
+        }
+
+        internal void AsocAn_StudiuSemestru(int semID, int an_studiuID)
+        {
+
+            semDAL.AsocAn_StudiuSemestru(semID, an_studiuID);
+        }
+    }
+}
