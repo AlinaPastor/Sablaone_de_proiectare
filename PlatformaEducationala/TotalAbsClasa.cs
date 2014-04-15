af20667dd32537db77fae65348cebfa3ed37f633
AlinaPastor <pastor_alina_ada@yahoo.com>
2014-04-15 19:55:46 +0300

    Defineste interfata utilizator pentru cadrul didactic ce afiseaza numarul total de absente per clasa la o anumita disciplina.

---
+++ PlatformaEducationala/TotalAbsClasa.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace PlatformaEducationala
+{
+    public partial class TotalAbsClasa : Form
+    {
+        public TotalAbsClasa(DataSet nrAbs)
+        {
+            InitializeComponent();
+            Afiseaza( nrAbs);
+        }
+
+
+        private void Afiseaza(DataSet nrAbs)
+        {
+
+            labelNrAbsCls.Text = nrAbs.Tables[0].Rows[0][0].ToString();
+        }
+
+        private void buttonCancelVizAbs_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
