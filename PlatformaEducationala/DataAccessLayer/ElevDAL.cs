7b64001eab8813631f2246d3f7926dea9df49874
Oana Dumitrasc <oana.dumitrasc@yahoo.com>
2014-05-04 22:48:49 +0300

    Modificari clase

--- PlatformaEducationala/DataAccessLayer/ElevDAL.cs
+++ PlatformaEducationala/DataAccessLayer/ElevDAL.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Data;
 using System.Data.SqlClient;
+using System.Data.Common;
 
 namespace PlatformaEducationala.DataAccessLayer
 {
@@ -12,99 +13,138 @@ namespace PlatformaEducationala.DataAccessLayer
 
         DatabaseAccess db = new DatabaseAccess();
 
-        public DataSet GetElevi()
+        public DataTable GetElevi()
         {
-            //return db.ExecuteDataSet(DALHelper.Connection, "GetElevi", null);
-            return null;
+            return db.ExecuteDataSet(DALHelper.Connection, "GetElevi", null);
         }
 
-        public DataSet GetEleviFaraClasa()
+        public DataTable GetEleviFaraClasa()
         {
-           // return db.ExecuteDataSet(DALHelper.Connection, "GetEleviFaraClasa1", null);
-            return null;
+            return db.ExecuteDataSet(DALHelper.Connection, "GetEleviFaraClasa1", null);
         }
 
 
-        public DataSet GetEleviById(int elevID)
+        public DataTable GetEleviById(int elevID)
         {
-            /*
-            SqlParameter[] paramList = new SqlParameter[]
-            {
-                new SqlParameter("@elevID",elevID )
-            };
+            DbParameter[] paramList = new DbParameter[1];
+
+            DbParameter paramElev = DALHelper.ProviderFactory.CreateParameter();
+            paramElev.ParameterName = "@elevID";
+            paramElev.Value = elevID;
+
+            paramList[0] = paramElev;
+
             return db.ExecuteDataSet(DALHelper.Connection, "GetEleviById", paramList);
-             */
-            return null;
+             
         }
 
         internal void AddElev(string nume, string prenume, string user, string parola)
         {
-            /*
-            SqlParameter[] paramList = new SqlParameter[]
-            {
-                
-                new SqlParameter("@nume", nume),
-                new SqlParameter("@prenume", prenume),
-                new SqlParameter("@user", user),
-                new SqlParameter("@parola", parola)
+            DbParameter[] paramList = new DbParameter[1];
+            
+            DbParameter paramNume = DALHelper.ProviderFactory.CreateParameter();
+            paramNume.ParameterName = "@nume";
+            paramNume.Value = nume;
 
-            };
+            DbParameter paramPrenume = DALHelper.ProviderFactory.CreateParameter();
+            paramPrenume.ParameterName = "@prenume";
+            paramPrenume.Value = prenume;
+
+            DbParameter paramUser = DALHelper.ProviderFactory.CreateParameter();
+            paramUser.ParameterName = "@user";
+            paramUser.Value = user;
+
+            DbParameter paramParola = DALHelper.ProviderFactory.CreateParameter();
+            paramParola.ParameterName = "@parola";
+            paramParola.Value = parola;
+
+            paramList[0] = paramNume;
+            paramList[1] = paramPrenume;
+            paramList[2] = paramUser;
+            paramList[3] = paramParola;
+
             db.ExecuteNonQuery(DALHelper.Connection, "AddElev", paramList);
-             */
+             
         }
 
 
-        internal void UpdateElev(int id_elev, string nume, string prenume, string user, string parola)
+        internal void UpdateElev(int elevID, string nume, string prenume, string user, string parola)
         {
-            /*
-            SqlParameter[] paramList = new SqlParameter[]
-            {
-                new SqlParameter("@elevID", id_elev),
-                new SqlParameter("@nume", nume),
-                new SqlParameter("@prenume", prenume),
-                new SqlParameter("@user", user),
-                new SqlParameter("@parola", parola)
+            DbParameter[] paramList = new DbParameter[1];
+            
+            DbParameter paramIdElev = DALHelper.ProviderFactory.CreateParameter();
+            paramIdElev.ParameterName = "@elevID";
+            paramIdElev.Value = elevID;
 
-            };
+            DbParameter paramNume = DALHelper.ProviderFactory.CreateParameter();
+            paramNume.ParameterName = "@nume";
+            paramNume.Value = nume;
+
+            DbParameter paramPrenume = DALHelper.ProviderFactory.CreateParameter();
+            paramPrenume.ParameterName = "@prenume";
+            paramPrenume.Value = prenume;
+
+            DbParameter paramUser = DALHelper.ProviderFactory.CreateParameter();
+            paramUser.ParameterName = "@user";
+            paramUser.Value = user;
+
+            DbParameter paramParola = DALHelper.ProviderFactory.CreateParameter();
+            paramParola.ParameterName = "@parola";
+            paramParola.Value = parola;
+
+            paramList[0] = paramIdElev;
+            paramList[1] = paramNume;
+            paramList[2] = paramPrenume;
+            paramList[3] = paramUser;
+            paramList[4] = paramParola;
+
             db.ExecuteNonQuery(DALHelper.Connection, "UpdateElev", paramList);
-             */
         }
 
         internal void DeleteElev(int elevID)
         {
-            /*
-            SqlParameter[] paramList = new SqlParameter[]
-            {
-                new SqlParameter("@elevID", elevID)
-            };
+
+            DbParameter[] paramList = new DbParameter[1];
+
+            DbParameter paramIdElev = DALHelper.ProviderFactory.CreateParameter();
+            paramIdElev.ParameterName = "@elevID";
+            paramIdElev.Value = elevID;
+            
             db.ExecuteNonQuery(DALHelper.Connection, "DeleteElev", paramList);
-             */
+            
         }
 
-        internal void AsocElevClasa(int id_elev,int id_clasa)
+        internal void AsocElevClasa(int elevID,int clasaID)
         {
-            /*
-            SqlParameter[] paramList = new SqlParameter[]
-            {
-                new SqlParameter("@elevID", id_elev),
-                new SqlParameter("@clasaID", id_clasa)
-               
+            DbParameter[] paramList = new DbParameter[1];
 
-            };
+            DbParameter paramIdElev = DALHelper.ProviderFactory.CreateParameter();
+            paramIdElev.ParameterName = "@elevID";
+            paramIdElev.Value = elevID;
+
+            DbParameter paramIdClasa = DALHelper.ProviderFactory.CreateParameter();
+            paramIdClasa.ParameterName = "@clasaID";
+            paramIdClasa.Value = clasaID;
+
+            paramList[0] = paramIdElev;
+            paramList[1] = paramIdClasa;
+
             db.ExecuteNonQuery(DALHelper.Connection, "AsocElevClasa", paramList);
-             */
+
         }
 
         public DataSet GetEleviByClasaId(int clasaID)
         {
-            /*
-            SqlParameter[] paramList = new SqlParameter[]
-            {
-                new SqlParameter("@clasaID",clasaID )
-            };
+            DbParameter[] paramList = new DbParameter[1];
+
+            DbParameter paramIdClasa = DALHelper.ProviderFactory.CreateParameter();
+            paramIdClasa.ParameterName = "@clasaID";
+            paramIdClasa.Value = clasaID;
+
+            paramList[1] = paramIdClasa;
+            
             return db.ExecuteDataSet(DALHelper.Connection, "GetEleviByClasaId", paramList);
-             */
-            return null;
+            
         }
 
     }
