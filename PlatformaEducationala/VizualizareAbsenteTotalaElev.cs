b77fdf47dc9e63e52676d18f4189a443d0987f2e
Oana Dumitrasc <oana.dumitrasc@yahoo.com>
2014-05-04 15:22:18 +0300

    Vizualizare numar total absente.

---
+++ PlatformaEducationala/VizualizareAbsenteTotalaElev.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace PlatformaEducationala
+{
+    public partial class VizualizareAbsenteTotalaElev : Form
+    {
+        public VizualizareAbsenteTotalaElev(DataSet absente)
+        {
+            InitializeComponent();
+            Afiseaza(absente);
+        }
+
+        private void Afiseaza(DataSet listaAbs)
+        {
+            for (int i = 0; i < listaAbs.Tables[0].Rows.Count; i++)
+            {
+                String absenta = "Data: " + listaAbs.Tables[0].Rows[i]["data_absenta"].ToString() + " Motivata: "
+                                 + listaAbs.Tables[0].Rows[i]["motivata"].ToString() + " Motivabila: "
+                                 + listaAbs.Tables[0].Rows[i]["motivabila"].ToString();
+                listBoxAbsenteVizualizare.Items.Add(absenta);
+            }
+        }
+
+        private void buttonCancelVizAbs_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
